4b018e35861d9927f547f62e5d5ea65a175d6e04
minmario <minmario@korea.ac.kr>
2025-08-11 17:28:57 +0900

    Initial commit

---
+++ toyerp/src/test/java/com/example/toyerp/ToyerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
