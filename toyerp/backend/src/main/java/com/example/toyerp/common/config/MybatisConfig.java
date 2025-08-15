7ac37ef1f842af9f806db845fa428d0a73391ce6
Seon Hong Min <21017030@suwon.ac.kr>
2025-08-15 17:30:11 +0900

    Revert "Minmario"

--- toyerp/backend/src/main/java/com/example/toyerp/common/config/MybatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.toyerp.common.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.example.toyerp.domain.**.mapper")
-public class MybatisConfig {
-}
