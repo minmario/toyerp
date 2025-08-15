12bcedec7939c3ed1fec4bdabb5b0ce4f627ac00
minmario <minmario@korea.ac.kr>
2025-08-15 17:23:36 +0900

    look

--- toyerp/backend/src/main/java/com/example/toyerp/common/config/MybatisConfig.java
+++ toyerp/backend/src/main/java/com/example/toyerp/common/config/MybatisConfig.java
@@ -1,5 +1,9 @@
+package com.example.toyerp.common.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
-@MapperScan(basePackages = "com.example.toyerp.account.mapper")
-public class MyBatisConfig {
-    // 추가 설정이 필요 없으면 빈 파일이어도 됩니다.
+@MapperScan(basePackages = "com.example.toyerp.domain.**.mapper")
+public class MybatisConfig {
 }
